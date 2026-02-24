534a524c3de54d901232804036540c5f3a198b11
KashishZalavadi735 <kashishzalavadi@gmail.com>
2026-02-24 20:21:16 +0530

    Changes sendEmail.ts

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -17,6 +17,9 @@ export const sendEmail = async (
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    tls: {
+      rejectUnauthorized: false,
+    },
   });
 
   return transporter.sendMail({
