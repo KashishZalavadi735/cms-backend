6c0ab3abc7bd0aa5a05227661b24d552aa57e1a5
KashishZalavadi735 <kashishzalavadi@gmail.com>
2026-02-28 15:57:06 +0530

    Changes in sendEmail file using brevo

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,18 +1,15 @@
 import nodemailer from "nodemailer";
 
-export const sendEmail = async (
-  to: string,
-  subject: string,
-  html: string,
-) => {
+export const sendEmail = async (to: string, subject: string, html: string) => {
   const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
+    port: 465, // 🔥 CHANGE
+    secure: true, // 🔥 MUST be true
     auth: {
       user: "apikey",
       pass: process.env.BREVO_SMTP_KEY,
     },
+    connectionTimeout: 20_000, // 🔥 prevent timeout
   });
 
   return transporter.sendMail({
