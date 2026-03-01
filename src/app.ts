04a7a93e4f501d024957fef92cdc848a45439a17
KashishZalavadi735 <kashishzalavadi@gmail.com>
2026-03-01 10:01:27 +0530

    Changes in app file for base route

--- src/app.ts
+++ src/app.ts
@@ -29,6 +29,12 @@ app.use(cors({
 // Image import
 app.use("/public", express.static(path.join(__dirname, "../public")));
 
+// ROOT 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "src", "index.html"));
+});
+
+
 app.use(express.json());
 
 // Routes
