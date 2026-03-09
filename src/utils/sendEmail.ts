7f5e62896adbed6b1197e3681d042ad3bb8558b3
KashishZalavadi735 <kashishzalavadi@gmail.com>
2026-03-09 12:02:54 +0530

    Changes in sendmail file

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,47 +1,19 @@
 import nodemailer from "nodemailer";
-import { google } from "googleapis";
 
-const OAuth2 = google.auth.OAuth2;
+export const sendEmail = async (to: string, subject: string, htmlContent: string) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-const oauth2Client = new OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET
-);
-
-oauth2Client.setCredentials({
-  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-});
-
-export const sendEmail = async (
-  to: string,
-  subject: string,
-  htmlContent: string
-): Promise<void> => {
-  try {
-    const accessToken = await oauth2Client.getAccessToken();
-
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-        accessToken: accessToken.token || "",
-      },
-    });
-
-    const info = await transporter.sendMail({
-      from: `"CMS Admin" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html: htmlContent,
-    });
-
-    console.log("Email sent:", info.response);
-  } catch (error) {
-    console.error("Email sending failed:", error);
-    throw error;
-  }
+  return transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html: htmlContent, // Send HTML instead of plain text
+    text: "Please view this email in HTML-supported client",
+  });
 };
